2e7a3b885e85461a7d68a1f5eaf70924bac8bb32
Habib <habibsan01@gmail.com>
2023-09-24 23:11:54 +0700

    connect to db

--- index.ts
+++ index.ts
@@ -1,38 +1,101 @@
 import express, { Request, Response } from "express";
-import iTodos from "./src/interface/Todos";
-import Todos from "./src/mocks/Todos";
+import dotenv from "dotenv";
+import pool from "./src/config/db";
 
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.APP_PORT;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response): Response => {
-  return res.status(200).json({ message: "OK" });
+app.get("/todos", async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const allTodos = await pool.query(
+      "SELECT * FROM todo ORDER BY todo_id ASC;"
+    );
+    return res.status(200).json(allTodos.rows);
+  } catch (error) {
+    return res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-app.get("/todos", (req: Request, res: Response): Response => {
-  return res.status(200).json({ data: Todos });
+app.get("/todo/:id", async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+
+    const todoGet = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    if (todoGet.rows.length === 0)
+      return res.status(404).json({ Error: "Todo not found" });
+
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    return res.status(200).json(todo.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "Something error when find one" });
+  }
 });
 
-app.get("/todo/:id", (req: Request, res: Response): Response => {
-  const { id } = req.params;
-  const data = Todos.find((data) => data.id === Number(id));
-  return res.status(200).json({ data });
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    return res.status(200).json(newTodo.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "Something error when POST" });
+  }
 });
 
-app.post("/todos", (req: Request, res: Response): Response => {
-  const data: iTodos = req.body;
-  Todos.push(data);
+app.put("/todo/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
 
-  return res.status(201).json({ data: Todos });
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    return res.status(200).json("Todo was updated successfully");
+  } catch (error) {
+    return res.status(500).json({ message: "Something error when PUT" });
+  }
 });
 
-app.delete("/todo/:id", (req: Request, res: Response): Response => {
-  const { id } = req.params;
-  const data: iTodos[] = Todos.filter((data) => data.id !== Number(id));
-  return res.status(200).json({ data });
-});
+app.delete(
+  "/todo/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const todoToDelete = await pool.query(
+        "SELECT * FROM todo WHERE todo_id = $1",
+        [id]
+      );
+
+      if (todoToDelete.rows.length === 0)
+        return res.status(404).json({ Error: "Todo not found" });
+
+      const deleteTodo = await pool.query(
+        "DELETE from todo WHERE todo_id = $1",
+        [id]
+      );
+
+      return res.status(200).json("Todo was updated successfully");
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Something error while delete todo" });
+    }
+  }
+);
 
 async function start(): Promise<void> {
   try {
--- src/interface/Todos.ts
+++
@@ -1,5 +0,0 @@
-export default interface iTodos {
-    id: number
-    title: string
-    isDone: boolean
-}
